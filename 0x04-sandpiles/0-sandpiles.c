30f5e63307c239e449a02e547ed805474eed721e
Juanpagab99 <juanpagab99@gmail.com>
2022-01-14 14:29:15 +0700

    task 0 fix

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,111 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * addition - add 2 grids
+ * @grid1: first matrix
+ * @grid2: second matrix
+ */
+void addition(int grid1[3][3], int grid2[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] = grid1[i][j] + grid2[i][j];
+		}
+	}
+}
+/**
+ * print - print a grid
+ * @grid: matrix
+ */
+void grid_print(int grid[3][3])
+{
+	int i = 0;
+	int j = 0;
+
+	printf("=\n");
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
+}
+/**
+ * ok - check if a grid is ok
+ * @grid: matrix
+ * Return: 1 if is ok or 0 if not
+ */
+int ok(int grid[3][3])
+{
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+				return (0);
+		}
+	}
+	return (1);
+}
+/**
+ * grid_ch - change a grid value
+ * @grid1: matrix
+ */
+void grid_ch(int grid1[3][3])
+{
+	int i = 0;
+	int j = 0;
+	int gridx[3][3];
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+			gridx[i][j] = 0;
+	}
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid1[i][j] > 3)
+			{
+				grid1[i][j] = grid1[i][j] - 4;
+				if ((i - 1) >= 0 && (i - 1) < 3)
+					gridx[i - 1][j] += 1;
+				if ((j - 1) >= 0 && (j - 1) < 3)
+					gridx[i][j - 1] += 1;
+				if ((i + 1) >= 0 && (i + 1) < 3)
+					gridx[i + 1][j] += 1;
+				if ((j + 1) >= 0 && (j + 1) < 3)
+					gridx[i][j + 1] += 1;
+			}
+		}
+	}
+	addition(grid1, gridx);
+}
+/**
+ * sandpiles_plus - sum 2 sandpiles
+ * @grid1: first matrix
+ * @grid2: second matrix
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+	addition(grid1, grid2);
+	while (!ok(grid1))
+	{
+		grid_print(grid1);
+		grid_ch(grid1);
+	}
+}
